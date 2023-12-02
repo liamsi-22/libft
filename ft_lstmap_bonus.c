6717919ac1f03da2150a0473dcd6dab30594ec62
iel-fagh <iel-fagh@c4r2p8.1337.ma>
2023-12-02 14:29:14 +0100

    done

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,12 +6,28 @@
 /*   By: iel-fagh <iel-fagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:21:01 by iel-fagh          #+#    #+#             */
-/*   Updated: 2023/12/01 21:13:50 by iel-fagh         ###   ########.fr       */
+/*   Updated: 2023/12/02 14:28:02 by iel-fagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
+static void	ft_dell(void *x)
+{
+	(void)x;
+	return ;
+}
+static void *ft_f(void *str)
+{
+	int i = 0;
+	while (((char *)str)[i] != '\0')
+	{
+		((char *)str)[i] -= 32;
+		i++;		
+	}
+	return ((void *)str);
+}
+*/
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
@@ -20,6 +36,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
+	new_node = NULL;
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst -> content));
@@ -33,3 +50,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_list);
 }
+/*
+int main ()
+{
+	char y[] = "hello";
+	char x[] = "world";
+	t_list *new_elem;
+	new_elem = ft_lstnew(x);
+	new_elem -> next = ft_lstnew(y);
+	t_list *mapp = ft_lstmap(new_elem, ft_f, ft_dell);
+#include "libft.h"
+*/
